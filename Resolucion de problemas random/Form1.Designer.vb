9d050abf3686e01717b6ca47a37209c3e91105a3
nicob <nicob@DESKTOP-V1FKR91>
2023-11-07 23:26:32 -0600

    Agregar archivos de proyecto.

---
+++ Resolucion de problemas random/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnVerificar = New System.Windows.Forms.Button()
+        Me.txtRespuesta = New System.Windows.Forms.TextBox()
+        Me.lblProblema = New System.Windows.Forms.Label()
+        Me.lblResultado = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.lblnombre = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnVerificar
+        '
+        Me.btnVerificar.BackColor = System.Drawing.Color.Gold
+        Me.btnVerificar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnVerificar.Font = New System.Drawing.Font("Microsoft Tai Le", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnVerificar.Location = New System.Drawing.Point(99, 223)
+        Me.btnVerificar.Name = "btnVerificar"
+        Me.btnVerificar.Size = New System.Drawing.Size(108, 53)
+        Me.btnVerificar.TabIndex = 0
+        Me.btnVerificar.Text = "Verificar"
+        Me.btnVerificar.UseVisualStyleBackColor = False
+        '
+        'txtRespuesta
+        '
+        Me.txtRespuesta.Location = New System.Drawing.Point(44, 156)
+        Me.txtRespuesta.Multiline = True
+        Me.txtRespuesta.Name = "txtRespuesta"
+        Me.txtRespuesta.Size = New System.Drawing.Size(217, 33)
+        Me.txtRespuesta.TabIndex = 1
+        '
+        'lblProblema
+        '
+        Me.lblProblema.AutoSize = True
+        Me.lblProblema.Location = New System.Drawing.Point(133, 133)
+        Me.lblProblema.Name = "lblProblema"
+        Me.lblProblema.Size = New System.Drawing.Size(39, 13)
+        Me.lblProblema.TabIndex = 2
+        Me.lblProblema.Text = "Label1"
+        '
+        'lblResultado
+        '
+        Me.lblResultado.AutoSize = True
+        Me.lblResultado.Location = New System.Drawing.Point(323, 133)
+        Me.lblResultado.Name = "lblResultado"
+        Me.lblResultado.Size = New System.Drawing.Size(0, 13)
+        Me.lblResultado.TabIndex = 3
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Gold
+        Me.Panel1.Controls.Add(Me.lblnombre)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(512, 88)
+        Me.Panel1.TabIndex = 4
+        '
+        'lblnombre
+        '
+        Me.lblnombre.AutoSize = True
+        Me.lblnombre.Font = New System.Drawing.Font("Microsoft Tai Le", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnombre.Location = New System.Drawing.Point(3, 9)
+        Me.lblnombre.Name = "lblnombre"
+        Me.lblnombre.Size = New System.Drawing.Size(396, 27)
+        Me.lblnombre.TabIndex = 5
+        Me.lblnombre.Text = "Resolucion de problemas matematicos "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkOrange
+        Me.ClientSize = New System.Drawing.Size(512, 341)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.lblResultado)
+        Me.Controls.Add(Me.lblProblema)
+        Me.Controls.Add(Me.txtRespuesta)
+        Me.Controls.Add(Me.btnVerificar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnVerificar As Button
+    Friend WithEvents txtRespuesta As TextBox
+    Friend WithEvents lblProblema As Label
+    Friend WithEvents lblResultado As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents lblnombre As Label
+End Class
---
+++ Resolucion de problemas random/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System
+
+Public Class Form1
+    Dim rand As New Random()
+    Dim num1 As Integer
+    Dim num2 As Integer
+    Dim operacion As String
+    Dim respuesta As Double ' Cambiado a Double para manejar divisiones con decimales
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        GenerarProblema()
+    End Sub
+
+    Private Sub GenerarProblema()
+        ' Generar dos numeros aleatorios y una operacion aleatoria
+        num1 = rand.Next(1, 101)
+        num2 = rand.Next(1, 101)
+
+        Dim operacionRandom = rand.Next(4) ' Generar un numero aleatorio entre el 0 y 3
+
+        ' Asigna la operacion segun el numero aleatorio
+        Select Case operacionRandom
+            Case 0
+                operacion = "+"
+            Case 1
+                operacion = "-"
+            Case 2
+                operacion = "*"
+            Case 3
+                operacion = "/"
+        End Select
+
+        ' Muestra el problema en el lbl
+        lblProblema.Text = $"{num1} {operacion} {num2} = ?"
+    End Sub
+
+    Private Sub btnVerificar_Click(sender As Object, e As EventArgs) Handles btnVerificar.Click
+        ' Obtiene la respuesta que se ingresa por el usuario desde el txtbox
+        If Double.TryParse(txtRespuesta.Text, respuesta) Then
+            ' Calcula la respuesta correcta
+            Dim respuestaCorrecta As Double
+
+            Select Case operacion
+                Case "+"
+                    respuestaCorrecta = num1 + num2
+                Case "-"
+                    respuestaCorrecta = num1 - num2
+                Case "*"
+                    respuestaCorrecta = num1 * num2
+                Case "/"
+                    ' Verificar si el divisor es cero antes de la división
+                    If num2 = 0 Then
+                        lblResultado.Text = "Divisor no puede ser cero."
+                        Return
+                    End If
+                    respuestaCorrecta = num1 / num2
+            End Select
+
+            ' Verificar si la respuesta es correcta
+            If respuesta = respuestaCorrecta Then
+                lblResultado.Text = "¡Correcto!"
+            Else
+                lblResultado.Text = "Incorrecto. La respuesta es " & respuestaCorrecta
+            End If
+
+            ' Generar un nuevo problema
+            GenerarProblema()
+
+            ' Limpia el txtbox
+            txtRespuesta.Clear()
+        Else
+            lblResultado.Text = "Por favor, ingresa un número válido."
+        End If
+    End Sub
+End Class
+
+
